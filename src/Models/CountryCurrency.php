3f65a6c30a71d0c0dc21269bdb01a2bdc3ad8f53
Landr Rakotoarivelo <folojona@gmail.com>
2020-09-19 11:57:25 +0200

    country currencies

---
+++ src/Models/CountryCurrency.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ry\Shop\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountryCurrency extends Model
+{
+    protected $table = "ry_shop_country_currencies";
+
+    public $timestamps = true;
+}
